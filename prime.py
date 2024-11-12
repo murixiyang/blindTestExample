e3d763d9d444f4ca5bc2000f9966257ecad1289a
Robert Chatley <robert@develogical.com>
2024-11-12 14:42:01 +0000

    Lydia was right.
    Co-authored-by: Lucanchvans <snow20021210@hotmail.com>

--- prime.py
+++ prime.py
@@ -4,7 +4,7 @@ def is_prime(num):
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
-    return False
+    return True
 
 def print_prime(n):
     primes = []
